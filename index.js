7019cb7ac7ed07b737f2ea143644d5b8dba4a832
pegah <pegahsafaie@gmail.com>
2021-03-08 12:18:54 +0100

    set default for cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const morgan = require('morgan')
 const port = process.env.PORT || 4444
 
 app.use(cors({
-  origin: process.env.portfolio
+  origin: process.env.portfolio || 'https://www.pegahsafaie.dev'
 }));
 app.use(morgan('dev'))
 app.use(express.json())
